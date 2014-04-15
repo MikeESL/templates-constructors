15a3d7a1c63a3d9134bc2b102441c887ae9f41fa
MikeESL <mike.reser.03@gmail.com>
2014-04-15 15:50:22 -0400

    4th commit

--- js/script.js
+++ js/script.js
@@ -13,7 +13,7 @@ var instagrams = [
 },
 ];
 
-var template = function(instaData) {
+var instagramsTemplate = function(instaData) {
 		var markup = "";
 		var i;
 
@@ -22,5 +22,40 @@ var template = function(instaData) {
 }
 		return markup;
 };
-var popularGrams = template(instagrams);
+var popularGrams = instagramsTemplate(instagrams);
 $(".template1").append(popularGrams);
+
+//template 2
+
+var photos = [
+{
+	image: "<img src=\"images/falls.png\">",
+	desc: "Looking Glass Falls",
+	author: "by Sabbath Lily Hawks",
+},
+{	image: "<img src=\"images/faucet.png\">",
+	desc: "Faucet",
+	author: "by Hazel Motes",
+},
+{	image: "<img src=\"images/dog.png\">",
+	desc: "Maggie",
+	author: "by Enoch Emory",
+},
+{	image: "<img src=\"images/sunrise3.png\">",
+	desc: "Lake Marion Sunrise",
+	author: "by Hoover Shoats",
+}
+];
+var photosTemplate = function(photodata) {
+		var markup = "";
+		var i;
+
+		for ( i=0; i<photodata.length; i++){
+			markup += "<div>" + photodata[i].image + "</div><h3>" + photodata[i].desc + "</h3><h4>" + photodata[i].author + "</h4>";
+}
+		return markup;
+};
+var popularPhotos = photosTemplate(photos);
+$(".template2").append(popularPhotos);
+
+
