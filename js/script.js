87aa337f729471b00f5e23384d996134d427aa13
MikeESL <mike.reser.03@gmail.com>
2014-04-15 14:17:15 -0400

    2nd commit

---
+++ js/script.js
@@ -0,0 +1,26 @@
+//template 1
+
+var instagrams = [
+{
+	user: "gospelhaunted",
+	photo: "<a href=\"http://instagram.com/p/kpMfI-jSfV/\">Folly Beach Pier</a>",
+},
+{	user:"keidrianraye",
+	photo: "a href=\"http://instagram.com/p/llV7zcun9S/\">Shem Creek</a>",
+},
+{	user: "stognasty",
+	photo:"a href=\"http://instagram.com/p/mAsri3K84G/\">Botany Bay</a>",
+},
+];
+
+var template = function(instaData) {
+		var markup " ";
+		var i;
+
+		for (i=0, i<instaData.length, i++){
+			markup += "<h2>" + data[i].user + "</h2><p>" + data[i].photo + "</p>";
+}
+		return markup;
+};
+var popularGrams = template(instagrams);
+$(".template1").append(popularGrams);
